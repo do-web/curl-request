8d4276ca401e354fdb2347ff3881194be5a31614
do-web <dweber@uandi.com>
2017-12-07 19:36:30 +0100

    Init

---
+++ README.md
@@ -0,0 +1,113 @@
+# cURL Request for Node.js
+
+[![License][npm-license]][license-url]
+[![Downloads][npm-downloads]][npm-url]
+[![Gitter][gitter-image]][gitter-url]
+
+cURL Request is a cURL wrapper, support all options and infos from cURL.
+
+## Why a cURL wrapper?
+
+* All node libaries i have tested are very buggy in case you use a proxy and https
+* cURL is working for all cases since years
+* The wrapper is easy to use with promises 
+
+## Installing
+
+To utilize for node.js install the the `npm` module:
+
+```bash
+$ npm install curl-request --save
+```
+
+After installing the `npm` package you can now start simplifying requests like so:
+
+```js
+const curl = require('curl-request');
+```
+
+If you have problems installing the dependencies, use this to build it from source:
+
+```bash
+$ npm install node-libcurl --build-from-source 
+```
+
+## Useage
+
+```js
+
+const curl = require( 'curl-request' );
+
+curl.setHeaders([
+    'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+])
+.get('https://www.google.com')
+.then(({statusCode, body, headers}) => {
+    console.log(statusCode, body, headers)
+})
+.catch((e) => {
+    console.log(e);
+});
+
+curl.post('https://www.google.com')
+.setBody({
+ 'input-arr[0]': 'input-arr-val0',
+ 'input-arr[1]': 'input-arr-val1',
+ 'input-arr[2]': 'input-arr-val2',
+ 'input-name': 'input-val'
+})
+.then(({statusCode, body, headers}) => {
+    console.log(statusCode, body, headers)
+})
+.catch((e) => {
+    console.log(e);
+});
+
+// File upload
+curl.post('https://www.google.com')
+.setHeaders([
+    'Content-Type: multipart/form-data'
+])
+.setMultipartBody([{
+  name: 'filename',
+  contents: 'yourimage.png'
+}, {
+  name: 'file',
+  file: './yourimage.png',
+  type: 'image/png'
+}])
+.then(({statusCode, body, headers}) => {
+    console.log(statusCode, body, headers)
+})
+.catch((e) => {
+    console.log(e);
+});
+
+// Set tor proxy
+curl.setProxy('localhost:9050');
+
+// New tor identity
+curl.newTorIdentity();
+
+// Verbose output
+curl.setVerbose(true);
+
+```
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+
+----
+
+[license-url]: https://github.com/do-web/curl-request/blob/master/LICENSE
+
+[npm-url]: https://www.npmjs.com/package/curl-request
+[npm-license]: https://img.shields.io/npm/l/curl-request.svg?style=flat
+[npm-version]: https://badge.fury.io/js/curl-request.svg
+[npm-downloads]: https://img.shields.io/npm/dm/curl-request.svg?style=flat
---
+++ index.js
@@ -0,0 +1,153 @@
+const Curl = require('node-libcurl').Curl;
+const Net = require('net');
+const querystring = require('querystring');
+
+module.exports = (function () {
+
+    let that = this;
+
+    this['default'] = {
+        torControlHost: 'localhost',
+        torControlPort: 9051,
+        autoParse: true // content-type detect -> json
+    };
+
+    this.curl = new Curl();
+
+    this.newTorIdentity = () => {
+        let client = new Net.Socket();
+        client.connect(this['default'].torControlPort, this['default'].torControlHost, () => {
+            client.write('authenticate\nsignal newnym\nquit');
+        });
+    };
+
+    this._setUrl = (url) => {
+        this.curl.setOpt(Curl.option.URL, url);
+        return this;
+    };
+
+    this.setMultipartBody = (fieldsArray) => {
+        this.curl.setOpt(Curl.option.HTTPPOST, fieldsArray);
+        return this;
+    };
+
+    this.setOpt = (opt, val) => {
+        this.curl.setOpt(opt, val);
+        return this;
+    };
+
+    this.getCurl = () => {
+        return this.curl;
+    };
+
+    this.setCurl = (curl_replace) => {
+        this.curl = curl_replace;
+        return this;
+    };
+
+    this.setBody = (fieldsObj) => {
+        curl.setOpt(Curl.option.POSTFIELDS, querystring.stringify(fieldsObj));
+        return this;
+    };
+
+    this.setVerbose = (verbose) => {
+        this.curl.setOpt(Curl.option.VERBOSE, verbose);
+        return this;
+    };
+
+    this.setProxy = (host, proxyType) => {
+        this.curl.setOpt(Curl.option.PROXY, host);
+
+        // SOCKS5 default
+        if (typeof proxyType === 'undefined') {
+            this.curl.setOpt(Curl.option.PROXYTYPE, Curl.proxy.SOCKS5_HOSTNAME);
+        } else {
+            this.curl.setOpt(Curl.option.PROXYTYPE, proxyType);
+        }
+
+        return this;
+    };
+
+    this.setFollowLocation = (followlocation) => {
+        this.curl.setOpt(Curl.option.FOLLOWLOCATION, followlocation);
+        return this;
+    };
+
+    this.setHeaders = (headers) => {
+        this.curl.setOpt(Curl.option.HTTPHEADER, headers);
+        return this;
+    };
+
+    this.get = (url) => {
+        this._setUrl(url);
+        this.curl.setOpt(Curl.option.CUSTOMREQUEST, 'GET');
+        return this._submit();
+    };
+
+    this.post = (url) => {
+        this._setUrl(url);
+        this.curl.setOpt(Curl.option.CUSTOMREQUEST, 'POST');
+        return this._submit();
+    };
+
+    this.patch = (url) => {
+        this._setUrl(url);
+        this.curl.setOpt(Curl.option.CUSTOMREQUEST, 'PATCH');
+        return this._submit();
+    };
+
+    this['delete'] = (url) => {
+        this._setUrl(url);
+        this.curl.setOpt(Curl.option.CUSTOMREQUEST, 'DELETE');
+        return this._submit();
+    };
+
+    this.head = (url) => {
+        this._setUrl(url);
+        this.curl.setOpt(Curl.option.CUSTOMREQUEST, 'HEAD');
+        return this._submit();
+    };
+
+    let normalizeHeaders = (headers) => {
+        // normalize headers
+        let nHeaders = {};
+        if (headers.length) {
+            Object.keys(headers[0]).forEach((k) => {
+                nHeaders[k.toString().toLocaleLowerCase()] = headers[0][k];
+            });
+        }
+        return nHeaders;
+    };
+
+    this._submit = () => {
+        return new Promise((resolve, reject) => {
+
+            this.curl.on('end', function (statusCode, body, headers) {
+
+                headers = normalizeHeaders(headers);
+
+                if (that.default.autoParse) {
+                    if (typeof headers['content-type'] !== 'undefined' &&
+                        headers['content-type'].toLocaleLowerCase() === 'application/json') {
+                        try {
+                            let jsonObj = JSON.parse(body);
+                            body = jsonObj;
+                        } catch (e) {
+                        }
+                    }
+                }
+
+                resolve({statusCode, body, headers});
+                this.close();
+            });
+
+            this.curl.on('error', function () {
+                reject(arguments);
+                this.close();
+            });
+            this.curl.perform();
+        });
+    };
+
+    return this;
+})();
